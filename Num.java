6836bfc095c95094e8d2d94c938579788b91207e
sakshi-sanjay-ingale <ingalesakshi458@gmail.com>
2025-07-31 11:51:03 +0530

    Sum of numbers

---
+++ Num.java
@@ -0,0 +1,12 @@
+ public class Num{
+ public static void main(String[] args)
+ {
+   int sum=0;
+    int n=10;
+for(int i=1;i<= n ;i++)
+{
+  sum=sum+i;
+}
+System.out.println("sum of" +n+ "num" + sum);
+       }
+     }
