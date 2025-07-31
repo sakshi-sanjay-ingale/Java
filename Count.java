78ef4f1187880f1578823298fa899b74a9d048e5
sakshi-sanjay-ingale <ingalesakshi458@gmail.com>
2025-07-31 11:56:52 +0530

    Count of total odd numbers between 1 to100

---
+++ Count.java
@@ -0,0 +1,11 @@
+public class Count {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 != 0) {
+                count++;
+            }
+        }
+        System.out.println("Total odd numbers from 1 to 100: " + count);
+    }
+}
