18fe2f2b2ffa3a1c2d344154d5476adfb138a1ec
sakshi-sanjay-ingale <ingalesakshi458@gmail.com>
2025-08-01 09:31:42 +0530

    Default constructor in java

---
+++ Constructor.java
@@ -0,0 +1,12 @@
+import java.io.*;
+ class Constructor{
+     
+    Constructor(){
+      System.out.println("default constructor");
+}
+  
+public static void main(String[] args)
+ {
+      Constructor hello = new Constructor();
+  }
+}
